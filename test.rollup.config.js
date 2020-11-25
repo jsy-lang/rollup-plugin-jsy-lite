f234a661dfde47d5117cb9567c7fbc8042740f7e
Shane Holloway <shane.holloway@ieee.org>
2020-11-25 10:07:52 -0700

    Fixes to bundling structure to pass travis testing

--- test.rollup.config.js
+++ test.rollup.config.js
@@ -1,4 +1,5 @@
-import rpi_jsy from 'rollup-plugin-jsy-lite'
+//import rpi_jsy from 'rollup-plugin-jsy-lite'
+import rpi_jsy from './esm/index.js'
 
 export default {
 	input: 'node_modules/jsy-transpile/code/scanner/inject_dedent.jsy',
